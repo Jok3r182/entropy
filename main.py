809a6beb5e98bccae62a1882d934a28e9058e276
Jok3r182 <71766496+Jok3r182@users.noreply.github.com>
2021-10-04 21:51:55 +0300

    normal grayscale added!

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from PIL import Image
+import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 
 def convert_to_grayscale(path):
-    img = Image.open(path)
-    return np.dot(img[..., :3], [0.299, 0.587, 0.144])
+    img = cv2.imread(path)
+    H, W, RGB = img.shape[:3]
+    gray = np.zeros((H, W), np.uint8)
+    for i in range(H):
+        for j in range(W):
+            gray[i, j] = np.sum(img[i, j]) / 3
+    return gray
 
 
 def find_image_entropy(grey_img):
