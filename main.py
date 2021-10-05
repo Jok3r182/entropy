e517869e3ef84c06e478a0c45ddaa27a8e3e27a2
Jok3r182 <71766496+Jok3r182@users.noreply.github.com>
2021-10-05 09:56:15 +0300

    added entropy calculation method!

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import os
+
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
 
+def find_probabilities(grey_img):
+    frequencies = np.zeros(256, np.uint8)
+    H, W = grey_img.shape[:2]
+    for i in range(H):
+        for j in range(W):
+            frequencies[grey_img[i, j]] += 1
+    probabilities = [frequency / np.sum(frequencies) for frequency in frequencies]
+    return [prob for prob in probabilities if prob > 0]
+
+
+def calculate_entropy(probabilities):
+    return np.sum([prob * np.log2(1.0 / prob) for prob in probabilities])
+
+
 def convert_to_grayscale(path):
     img = cv2.imread(path)
-    H, W, RGB = img.shape[:3]
+    H, W = img.shape[:2]
     gray = np.zeros((H, W), np.uint8)
     for i in range(H):
         for j in range(W):
@@ -13,11 +29,12 @@ def convert_to_grayscale(path):
     return gray
 
 
-def find_image_entropy(grey_img):
-    print(grey_img)
-
-
 if __name__ == '__main__':
-    greyImg = convert_to_grayscale('img.png')
-    plt.imshow(greyImg, cmap=plt.get_cmap('gray'))
-    plt.show()
+    for image_path in os.listdir('data'):
+        input_path = os.path.join('data', image_path)
+        greyImg = convert_to_grayscale(input_path)
+        imgPixelProbabilities = find_probabilities(greyImg)
+
+        print(image_path + ": " + str(calculate_entropy(imgPixelProbabilities)))
+        plt.imshow(greyImg, cmap=plt.get_cmap('gray'))
+        plt.show()
